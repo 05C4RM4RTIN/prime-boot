8f5cdaa455ed1e6106f8be424717ffa2c461a902
Hernandez Hernandez Oscar Martin <ohernandez-itseekers@cibanco.com>
2024-03-20 19:17:54 -0600

    Se sube el proyecto

---
+++ src/main/java/oscar_faces_boot/jsf/App.java
@@ -0,0 +1,13 @@
+package oscar_faces_boot.jsf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
